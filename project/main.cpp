164dee64d6afd85289e5d6527c3cecead94c24df
yessetovr <patek0325@gmail.com>
2022-09-18 15:43:26 +0600

    Initial Commit

---
+++ project/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  project
+//
+//  Created by Райымбек Есетов on 18.09.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
